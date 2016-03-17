dac44732a7f8605b840658a6b370802a69820093
Jessica <jess901108@gmail.com>
2016-03-17 02:07:56 +0000

    Finished Server.js part

--- server.js
+++ server.js
@@ -1,84 +1,32 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
+//declare dependencies
 
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 5000;
+var moment = require('moment');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
+var app = express();
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+app.use(express.static(path.resolve(__dirname,"client")));
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
+app.get("/:time",function(req,res){
+  var time = moment(req.params.time,"MMMM DD, YYYY",true);
+  if (!time.isValid()){
+    time = moment.unix(req.params.time)
+  }
+  
+  if (!time.isValid()){
+    res.json({
+      'natural':null,
+      'unix':null
     });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
+  }
+  
+  
+  res.json({
+    'natural':time.format("MMMM DD, YYYY"),
+    'unix':time.format('X')
   });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
+
+app.listen(port);
